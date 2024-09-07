7eeafe49a6b0f5f4fd6d48d2fb01dfade12ff4ed
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-09-08 04:54:56 +0800

    feat: made a webserver with express

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from 'express';
+import path from 'path';
+import url from 'url';
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('dis da app bro');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/about', (req, res) => {
-  res.send('about');
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(PORT, () => {
